9306ce9f93746fd885f57a71b189c21992050c90
Rafael Reis <therafaelreis@gmail.com>
2020-08-20 19:44:46 -0500

    :::change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changing the code.
+          Changing this as a test.
         </p>
         <a
           className="App-link"
